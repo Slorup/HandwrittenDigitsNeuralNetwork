b4bc3a19b2eb2ee3b89b8cf87e7f6fda58c7795f
Slorup <nicklas.slorup@yahoo.dk>
2020-09-18 15:39:38 +0200

    Change structure of eval, backpropagation is ongoing

--- src/main/java/NeuralNetwork.kt
+++ src/main/java/NeuralNetwork.kt
@@ -1,5 +1,4 @@
 import org.ejml.simple.SimpleMatrix
-import java.util.function.DoubleToLongFunction
 import kotlin.math.max
 import kotlin.random.Random
 
@@ -11,14 +10,16 @@ class NeuralNetwork {
                     weights[i] = Random.nextDouble(-1.0,1.0)
         }
 
-        fun zeroVersion(): Layer = Layer(SimpleMatrix(weights.numRows(), weights.numCols()), SimpleMatrix(bias.numRows(), 1))
+        val nodeCount = weights.numCols()
 
+        fun zeroVersion(): Layer = Layer(SimpleMatrix(weights.numRows(), weights.numCols()), SimpleMatrix(bias.numRows(), 1))
     }
 
     private val layers = listOf(
-        Layer(SimpleMatrix(16,IMAGE_SIZE), SimpleMatrix(16, 1), true),
-        Layer(SimpleMatrix(16,16), SimpleMatrix(16, 1), true),
-        Layer(SimpleMatrix(5, 16), SimpleMatrix(5,1), true)
+            Layer(SimpleMatrix(0,0), SimpleMatrix(0,0)),
+            Layer(SimpleMatrix(16,IMAGE_SIZE), SimpleMatrix(16, 1), true),
+            Layer(SimpleMatrix(16,16), SimpleMatrix(16, 1), true),
+            Layer(SimpleMatrix(5, 16), SimpleMatrix(5,1), true)
     )
 
     private fun relu(d: Double): Double = max(0.0, d)
@@ -35,29 +36,37 @@ class NeuralNetwork {
 
         val deltaLayers = layers.map { it.zeroVersion() }
 
-        for (l in layers.size - 1 downTo 0) {
-            val w = layers[l].weights
+
+        for (l in layers.reversed()) {
+            val w = l.weights
+
+            if (l == layers.last()) {
+
+            }
+
+
             for (i in 0 until w.numRows()) {
                 for (j in 0 until w.numCols()) {
 
                 }
             }
         }
-
+        return deltaLayers
     }
 
-    fun evaluate(id: ImageData): SimpleMatrix {
-        var activations = SimpleMatrix(IMAGE_SIZE, 1)
+    private fun evaluate(id: ImageData): Map<Layer, SimpleMatrix> {
+        val activations = mutableMapOf<Layer, SimpleMatrix>()
 
-        id.data.withIndex().forEach { activations[it.index] = it.value.toDouble() }
+        activations[layers.first()] = SimpleMatrix(layers.first().nodeCount, 1)
+        id.data.withIndex().forEach { activations[layers.first()]!![it.index] = it.value.toDouble() } // Initialize activations in first layer
 
-        for (l in layers) {
-            activations = l.weights.mult(activations).plus(l.bias)
-            for (i in 0 until activations.numRows())
-                activations[i] = relu(activations[i])
+        for ((current, previous) in layers.drop(1).withIndex().map { Pair(it.value, layers[it.index-1]) }) { // For each layer, multiply previous activations by the weights and add bias and apply the relu function: relu(a * w + b)
+            activations[current] = current.weights.mult(activations[previous]).plus(current.bias) // Calc a * w + b (also called z). li is incremented
+
+            for (i in 0 until activations[current]!!.numRows())
+                activations[current]!![i] = relu(activations[current]!![i]) // Apply relu function to each neuron on the new layer
         }
 
         return activations
     }
-
 }
